879c0101b5fda5386cc8fe9972810d9706df2be7
minsangkim <kmswin1@sktelecom.com>
2024-12-20 21:14:01 +0900

    add data

---
+++ run.sh
@@ -0,0 +1,3 @@
+python scripts/test_time_compute.py recipes/Llama-3.2-1B-Instruct/best_of_n.yaml
+python scripts/test_time_compute.py recipes/Llama-3.2-1B-Instruct/beam_search.yaml
+python scripts/test_time_compute.py recipes/Llama-3.2-1B-Instruct/dvts.yaml
